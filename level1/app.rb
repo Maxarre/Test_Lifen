6ae63113b490ec676ff070c88ab603cbe670ce04
Maxence Poncelet <maxenceponcelet@hotmail.com>
2019-06-24 18:34:01 +0200

    Level1 Test

--- level1/app.rb
+++ level1/app.rb
@@ -1,3 +1,41 @@
-require "json"
+require 'json'
 
-# votre code
+data = 'data.json'
+lecture = File.read(data)
+hash_lecture = JSON.parse(lecture, symbolize_names: true)
+
+xp_delivery = []
+totals = []
+
+pract = hash_lecture[:practitioners]
+comms = hash_lecture[:communications]
+
+pract.each do |practitioner|
+  if practitioner[:express_delivery]
+    xp_delivery << practitioner[:id]
+  end
+end
+
+comms_group = comms.group_by do |com|
+  com[:sent_at].split.first.to_s
+end
+comms_group.each do |day, communications|
+  price = communications.map do |communication|
+    comms_price = 0.1
+    comms_price += (communication[:pages_number] - 1) * 0.07
+    if communication[:color]
+      comms_price += 0.18
+    end
+    if xp_delivery.include? communication[:practitioner_id]
+      comms_price += 0.6
+    end
+    comms_price.to_f
+  end
+  totals.push(sent_on: day, total: price.sum.round(2))
+end
+
+new_output = { totals: totals }
+
+File.open('new_output.json', 'wb') do |output|
+  output.write(JSON.pretty_generate(new_output))
+end
--- level1/app_spec.rb
+++ level1/app_spec.rb
@@ -4,9 +4,36 @@ require 'rspec'
 
 require_relative './app.rb'
 
-RSpec.describe "Prices calculator", type: :model do
-  let(:data)          { JSON.parse(File.read('data.json')) }
-  let(:output)        { JSON.parse(File.read('output.json')) }
+RSpec.describe 'Prices calculator', type: :model do
+  let(:data) { JSON.parse(File.read('data.json')) }
+  let(:new_output) { JSON.parse(File.read('new_output.json')) }
+  let(:output) { JSON.parse(File.read('output.json')) }
 
-  pending("calculates prices")
+  it 'total of first day should be equal to 2.84' do
+    expect(new_output['totals'][0]['total']).to eq(2.84)
+  end
+
+  it 'total of second day should be equal to 1.47' do
+    expect(new_output['totals'][1]['total']).to eq(1.47)
+  end
+
+  it 'total of third day should be equal to 0.9' do
+    expect(new_output['totals'][2]['total']).to eq(0.9)
+  end
+
+  it 'total of fourth day should be equal to 1.82' do
+    expect(new_output['totals'][3]['total']).to eq(1.82)
+  end
+
+  it 'total of fith day should be equal to 2.1' do
+    expect(new_output['totals'][4]['total']).to eq(2.1)
+  end
+
+  it 'total of sixth day should be equal to 2.2' do
+    expect(new_output['totals'][5]['total']).to eq(2.2)
+  end
+
+  it 'new_output should equal output' do
+    expect(new_output).to eq(output)
+  end
 end
